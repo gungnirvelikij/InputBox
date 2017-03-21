48575fe8ef50b711d7ab8e8d4f2ff6cc9c4ec806
Christophe De Busschop <debusschopchristophe@gmail.com>
2017-03-21 20:29:03 +0100

    added class Inputbox + added fields + most methods

---
+++ InputBox/Inputbox.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputBox
+{
+    class Inputbox
+    {
+        private string input;
+        private string text;
+        private string title;
+
+        public Inputbox()
+        {
+            
+        }
+
+        public Inputbox(string title, string text)
+        {
+            setText(text);
+            setTitle(title);
+        }
+
+        public bool displayInputbox()
+        {
+            
+        }
+
+        public string getInput()
+        {
+            if (input != "" || input == null)
+            {
+                return input;
+            }
+            return "no input was given";
+        }
+
+        public void setTitle(string title)
+        {
+            this.title = title;
+        }
+
+        public void setText(string text)
+        {
+            this.text = text;
+        }
+
+        public string getTitle()
+        {
+            return title;
+        }
+
+        public string getText()
+        {
+            return text;
+        }
+    }
+}
