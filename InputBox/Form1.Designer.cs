4951539d79c7f20f1d1975f045547a080c82762d
Christophe De Busschop <debusschopchristophe@gmail.com>
2017-03-21 20:19:01 +0100

    Add project files.

---
+++ InputBox/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace InputBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblInputDescr = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(113, 61);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(49, 25);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(27, 13);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "Title";
+            // 
+            // lblInputDescr
+            // 
+            this.lblInputDescr.AutoSize = true;
+            this.lblInputDescr.Location = new System.Drawing.Point(49, 149);
+            this.lblInputDescr.Name = "lblInputDescr";
+            this.lblInputDescr.Size = new System.Drawing.Size(101, 13);
+            this.lblInputDescr.TabIndex = 5;
+            this.lblInputDescr.Text = "This is what we got:";
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(49, 64);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(60, 13);
+            this.lblText.TabIndex = 6;
+            this.lblText.Text = "Description";
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(49, 173);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(67, 13);
+            this.lblInput.TabIndex = 7;
+            this.lblInput.Text = "Your input ...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.lblInputDescr);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "InputBox calling window";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblInputDescr;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Label lblInput;
+    }
+}
+
